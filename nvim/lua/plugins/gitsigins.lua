aba78e2f1a4e0d27e6c33c6389add0c3303dcd7c
moriT958 <morit958@icloud.com>
2025-12-13 13:22:02 +0900

    gitsigns line blame をオン

--- nvim/lua/plugins/gitsigins.lua
+++ nvim/lua/plugins/gitsigins.lua
@@ -9,4 +9,13 @@ return {
       changedelete = { text = "~" },
     },
   },
+  config = function()
+    require("gitsigns").setup({
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 0,
+        virt_text_pos = 'eol',
+      }
+    })
+  end
 }
