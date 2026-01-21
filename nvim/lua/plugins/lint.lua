f2194ccd67002068f8707399fbb42677752fcf9d
morita <morit958@gmail.com>
2026-01-22 00:11:01 +0900

    turnoff shellcheck

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       local lint = require("lint")
       lint.linters_by_ft = {
-        zsh = { "shellcheck" },
+        -- zsh = { "shellcheck" },
       }
 
       local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
