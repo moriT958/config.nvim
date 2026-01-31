fcb29b038daee918de2f4a0a5faef7d000ac5455
morita <morit958@gmail.com>
2026-01-31 15:25:29 +0900

    Revert "oil.nvim の導入"
    This reverts commit 88badf3984016297bd69b66f71cf931349671b90.

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,38 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      window = {
+        position = "left",
+        width = 30,
+        auto_expand_width = false,
+        mappings = {
+          ["\\"] = "close_window",
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = true,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+          },
+          never_show = {},
+        },
+      },
+    })
+  end,
+  cmd = "Neotree",
+  keys = {
+    { "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+  },
+}
