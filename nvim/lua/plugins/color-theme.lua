ab46ed5cdaa4e9cfa128bca2af290cc6e44f786b
morita <morit958@gmail.com>
2026-02-04 20:35:37 +0900

    change color scheme (kanagawa dragon)

--- nvim/lua/plugins/color-theme.lua
+++ nvim/lua/plugins/color-theme.lua
@@ -1,25 +1,28 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "rebelot/kanagawa.nvim",
+  name = "kanagawa",
   priority = 1000,
-  opts = {},
   config = function()
-    require("catppuccin").setup({
-      transparent_background = true,
-      default_integrations = true,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
+    require("kanagawa").setup({
+      transparent = true, -- do not set background color
+      theme = "dragon", -- Load "wave" theme
+      background = { -- map the value of 'background' option to a theme
+        dark = "dragon", -- try "dragon" !
+        light = "lotus",
       },
-      custom_highlights = function(colors)
+      overrides = function(colors)
+        local theme = colors.theme
         return {
-          LineNr = { fg = colors.overlay0 },
+          TelescopeTitle = { fg = theme.ui.special, bold = true },
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
         }
       end,
     })
-    vim.cmd.colorscheme("catppuccin-macchiato")
-    -- vim.cmd.colorscheme("catppuccin-latte")
+    vim.cmd.colorscheme("kanagawa")
   end,
 }
